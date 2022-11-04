fd5bf9288a3b7f8134a3ac406588c2870196fed9
Sveta Rachok <sveta.r1212@gmail.com>
2022-11-04 23:24:15 +0300

    feat: add main page with boards list

--- project-management-app/src/app/material.module.ts
+++ project-management-app/src/app/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   exports: [
     MatButtonModule,
     MatSlideToggleModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
