b3b9bde3ecb78f8c0e09e56fcce372c936111555
deftonjke <vitali.saranchuk@gmail.com>
2022-11-06 00:57:29 +0100

    feat: add some material ui modules

--- project-management-app/src/app/material.module.ts
+++ project-management-app/src/app/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatInputModule,
+    MatCardModule
+  ],
+  exports: [MatInputModule, MatButtonModule, MatCardModule]
 })
 export class MaterialModule { }
