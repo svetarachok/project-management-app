232f7ffca7bb320eb697c19823e147da64d290aa
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 00:22:22 +0100

    fix: configuration devtools

--- project-management-app/src/app/app.module.ts
+++ project-management-app/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { BoardsModule } from './boards/boards.module';
 
 import { AppComponent } from './app.component';
 
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -24,7 +26,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     MaterialModule,
     StoreModule,
-    StoreDevtoolsModule,
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     AuthModule,
     CoreModule,
     UserModule,
