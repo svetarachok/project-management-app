549cb0778ebda120d2cbceb5219ced324c451859
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 00:23:23 +0100

    feat: add user reducers

---
+++ project-management-app/src/app/core/store/reducers/user.reducers.ts
@@ -0,0 +1,22 @@
+import { createReducer, on } from '@ngrx/store';
+
+import { initialUserState, UserState } from '../state/user.state';
+import * as UserActions from '../actions/user.actions';
+
+export const userReducer = createReducer(
+  initialUserState,
+  on(
+    UserActions.setAuth,
+    (state, { isAuth }): UserState => ({
+      ...state,
+      isAuth
+    })
+  ),
+  on(
+    UserActions.setUser,
+    (state, { user }): UserState => ({
+      ...state,
+      user
+    })
+  ),
+);
