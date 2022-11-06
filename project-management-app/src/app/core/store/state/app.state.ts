a976919f36abc00e0c02c78737b0b3431c4464fd
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 00:24:24 +0100

    feat: add user state to app state

--- project-management-app/src/app/core/store/state/app.state.ts
+++ project-management-app/src/app/core/store/state/app.state.ts
@@ -1,14 +1,17 @@
 import { BoardsState, initialBoardState } from "./boards.state";
+import { initialUserState, UserState } from "./user.state";
 // Import USer state and initialState
 
 // Add user state
 export interface AppState {
   boards: BoardsState,
+  user: UserState,
 }
 
-//add user initial 
+//add user initial
 export const initialAppState: AppState = {
   boards: initialBoardState,
+  user: initialUserState,
 }
 
 export function getAppState(): AppState {
