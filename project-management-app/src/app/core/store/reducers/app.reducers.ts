152fe889652293b45a45b979aa1c5c05051c7ede
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 00:23:51 +0100

    feat: add user reducer

--- project-management-app/src/app/core/store/reducers/app.reducers.ts
+++ project-management-app/src/app/core/store/reducers/app.reducers.ts
@@ -3,8 +3,10 @@ import { ActionReducerMap } from "@ngrx/store";
 import { AppState } from "../state/app.state";
 
 import { boardsReducer } from "./boards.reducers";
+import { userReducer } from "./user.reducers";
 
 
 export const appReducers: ActionReducerMap<AppState> = {
   boards: boardsReducer,
+  user: userReducer,
 }
