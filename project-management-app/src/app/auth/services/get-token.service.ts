c53ebaf7901e552e6ecbb3a3130db1a9f5d253bc
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 00:26:08 +0100

    feat: add auth status dispatch

--- project-management-app/src/app/auth/services/get-token.service.ts
+++ project-management-app/src/app/auth/services/get-token.service.ts
@@ -4,16 +4,21 @@ import { map, Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { TokenModel, User } from '../models/auth.model';
+import { Store } from '@ngrx/store';
+import { UserState } from '../../core/store/state/user.state';
+
+import * as UserActions from '../../core/store/actions/user.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetTokenService {
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private store: Store<UserState>) {}
 
   getToken(user: User): Observable<void> {
     return this.http.post<TokenModel>(environment.apiUrl + 'signin', user).pipe(map(({ token }) => {
       localStorage.setItem('team4-token', token);
+      this.store.dispatch(UserActions.setAuth({ isAuth: true }));
       this.router.navigateByUrl('/');
     }))
    }
