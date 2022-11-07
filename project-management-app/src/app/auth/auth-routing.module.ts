89a4e52e003d81dd60fad9e2cbc2d285ba3a3c7d
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 04:05:23 +0100

    feat: add login route

--- project-management-app/src/app/auth/auth-routing.module.ts
+++ project-management-app/src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 const routes: Routes = [
@@ -7,6 +8,7 @@ const routes: Routes = [
     path: 'sign-up',
     component: SignupPageComponent,
   },
+  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
