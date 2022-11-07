bb9f2b0feede43e22a6249b6b8a461fca22be9a5
deftonjke <vitali.saranchuk@gmail.com>
2022-11-07 04:05:01 +0100

    refactor: rename component

--- project-management-app/src/app/auth/auth.module.ts
+++ project-management-app/src/app/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { MaterialModule } from '../material.module';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AuthPageComponent, SignupPageComponent],
+  declarations: [LoginPageComponent, SignupPageComponent],
   imports: [
     CommonModule,
     MaterialModule,
--- project-management-app/src/app/auth/pages/login-page/login-page.component.spec.ts
+++ project-management-app/src/app/auth/pages/login-page/login-page.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthPageComponent } from './auth-page.component';
+import { LoginPageComponent } from './login-page.component';
 
 describe('AuthPageComponent', () => {
-  let component: AuthPageComponent;
-  let fixture: ComponentFixture<AuthPageComponent>;
+  let component: LoginPageComponent;
+  let fixture: ComponentFixture<LoginPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AuthPageComponent],
+      declarations: [LoginPageComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(AuthPageComponent);
+    fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
