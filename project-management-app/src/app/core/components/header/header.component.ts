4ad359c745ad7e26fbba223e400ba6ee6bc33d63
deftonjke <vitali.saranchuk@gmail.com>
2022-11-08 04:12:54 +0100

    feat: implement logout and conditional rendering

--- project-management-app/src/app/core/components/header/header.component.ts
+++ project-management-app/src/app/core/components/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { UserService } from '../../services/user.service';
+
+import * as fromUser from '../../store/selectors/user.selectors';
+import * as UserActions from '../../store/actions/user.actions';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  isUserAuthorized: boolean = true;
+export class HeaderComponent implements OnInit, OnDestroy {
+  isUserAuthorized: boolean = false;
+
+  private subs!: Subscription;
+
+  constructor(private store: Store, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.subs = this.store.select(fromUser.getIsAuth).subscribe(status => {
+      this.isUserAuthorized = status;
+    });
+  }
+
+  logout(): void {
+    this.userService.logout();
+    this.store.dispatch(UserActions.removeUser());
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
