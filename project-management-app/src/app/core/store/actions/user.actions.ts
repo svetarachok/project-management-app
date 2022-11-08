bbd1a36be8534f25d6e0c95d03dadd5e7521cfda
deftonjke <vitali.saranchuk@gmail.com>
2022-11-08 03:04:17 +0100

    feat: add actions and selectors

--- project-management-app/src/app/core/store/actions/user.actions.ts
+++ project-management-app/src/app/core/store/actions/user.actions.ts
@@ -3,7 +3,16 @@ import { User } from '../../models/user.model';
 
 const SET_AUTH = '[User] Set Auth';
 const SET_USER = '[User] Set User';
+const FETCH_USER = '[User] Fetch User';
+const FETCH_USER_FAILED = '[User] Fetch User Failed';
+const LOGOUT_USER = '[User] Logout User';
 
 export const setAuth = createAction(SET_AUTH, props<{ isAuth: boolean }>());
 
 export const setUser = createAction(SET_USER, props<{ user: User }>());
+
+export const fetchUser = createAction(FETCH_USER);
+
+export const fetchUserFailed = createAction(FETCH_USER_FAILED);
+
+export const logoutUser = createAction(LOGOUT_USER);
