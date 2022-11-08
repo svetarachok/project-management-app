54b6e1f7f258bbd788f8cde2a1913921638b33f2
deftonjke <vitali.saranchuk@gmail.com>
2022-11-08 03:02:55 +0100

    feat: add auth guard

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [AuthGuard],
   },
 ];
 
