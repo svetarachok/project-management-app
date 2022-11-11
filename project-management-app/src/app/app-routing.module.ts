dd04239c5ae2a92eb3aa565a2796359aa9a56b62
deftonjke <vitali.saranchuk@gmail.com>
2022-11-11 22:53:11 +0100

    feat: add guard

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { ExpCheckGuard } from './core/guards/exp-check.guard';
 import { NotAuthGuard } from './core/guards/not-auth.guard';
 import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 import { WelcomePageComponent } from './core/pages/welcome-page/welcome-page.component';
@@ -10,7 +11,7 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./boards/boards.module').then(m => m.BoardsModule),
-    canActivate: [NotAuthGuard],
+    canActivate: [NotAuthGuard, ExpCheckGuard],
   },
   {
     path: 'auth',
