5d912a1e10b6e6eda096926989bd5cdb2c90037e
Sveta Rachok <sveta.r1212@gmail.com>
2022-11-24 15:59:59 +0300

    fix: show only user tasks at search

--- project-management-app/src/app/core/services/search-service/search.service.ts
+++ project-management-app/src/app/core/services/search-service/search.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject, map, Subscription } from 'rxjs';
 import { Task } from '../../../boards/models/task.interface';
+import { getUserId } from '../../store/selectors/user.selectors';
+import { UserState } from '../../store/state/user.state';
 
 @Injectable({
   providedIn: 'root',
@@ -15,15 +18,30 @@ export class SearchService {
 
   searchSubscription!: Subscription;
 
-  constructor(private http: HttpClient) {}
+  userId: string = '';
+
+  userIdSubscription!: Subscription;
+
+  constructor(private http: HttpClient, private userStore: Store<UserState>) {}
 
   search(query: string) {
+    this.getUserId();
     return this.http
       .get<Task[]>('/tasksSet', {
         params: new HttpParams().set('search', query),
       })
-      .subscribe(res => {
-        this.searchResults.next([...res]);
-      });
+      .pipe(
+        map(result => {
+          result = result.filter(tasks => tasks.userId === this.userId);
+          return result;
+        })
+      )
+      .subscribe(res => this.searchResults.next([...(res as Task[])]));
+  }
+
+  getUserId() {
+    this.userIdSubscription = this.userStore
+      .select(getUserId)
+      .subscribe(id => (this.userId = id!));
   }
 }
